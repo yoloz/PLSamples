7ca9ae1d53eb418bea73218cb787d69cecddc91c
ylzhang <z88897050@gmail.com>
2018-04-13 16:31:28 +0800

    panic,recover了解

--- golang/cases/main.go
+++ golang/cases/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Z88897050/emulateSamples/golang/cases/qiniulog"
 	"github.com/Z88897050/emulateSamples/golang/cases/yamltest"
 	"github.com/Z88897050/emulateSamples/golang/cases/zaplog"
 )
 
+func createPanic() {
+	panic("create panic")
+}
+func recoverPanic(f func()) (b bool) {
+	defer func() {
+		if x := recover(); x != nil {
+			fmt.Println(x)
+		}
+	}()
+	f()
+	return
+}
 func main() {
 	zaplog.LogTest()
 	qiniulog.LogTest()
 	yamltest.YamlTest()
 
+	recoverPanic(createPanic)
 }
