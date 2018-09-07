a28dd9ff1e2f25d52c64195167450a12ee148ba8
ylzhang <z88897050@gmail.com>
2018-09-07 17:56:17 +0800

    一个进程的内存和系统cpu使用情况

---
+++ golang/process_performace.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/cloudfoundry/gosigar"
+)
+
+var (
+	running = true
+	oldCPU  sigar.Cpu
+)
+var process = flag.String("p", "", "get information about the specified process")
+
+//WaitForInterrupt wait for singl to close
+func WaitForInterrupt(interrupt func()) {
+
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM, os.Interrupt, os.Kill)
+
+	// Block until a signal is received.
+	s := <-c
+
+	log.Println("Receiving signal:", s)
+
+	interrupt()
+}
+
+// copyCPU copy cpu
+func copyCPU(target sigar.Cpu, source sigar.Cpu) {
+	target.User = source.User
+	target.Nice = source.Nice
+	target.Sys = source.Sys
+	target.Idle = source.Idle
+	target.Wait = source.Wait
+	target.Irq = source.Irq
+	target.SoftIrq = source.SoftIrq
+	target.Stolen = source.Stolen
+}
+func main() {
+	flag.Usage = func() {
+		fmt.Println(`
+			Usage:
+			process_performace -p 123
+			`)
+		os.Exit(0)
+	}
+	flag.Parse()
+	if *process == "" {
+		log.Fatal("pid is empty")
+	} else {
+		// cmd := exec.Command("top", "-b", "-n", "1", "-p", *process)
+		// stdout, err := cmd.StdoutPipe()
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// defer stdout.Close()
+		// if err := cmd.Start(); err != nil {
+		// 	log.Fatal(err)
+		// }
+		// opBytes, err := ioutil.ReadAll(stdout)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// log.Println(string(opBytes))
+		// if err := cmd.Wait(); err != nil {
+		// 	log.Printf("Command finished with error: %v", err)
+		// }
+		mem := sigar.ProcMem{}
+		// cpu := sigar.Cpu{}
+		// oldCPU := sigar.Cpu{}
+		// curCPU := sigar.Cpu{}
+		pid, err := strconv.Atoi(*process)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go WaitForInterrupt(func() { running = false })
+
+		for running {
+			// if oldCPU.Total() == 0 {
+			// 	if err := cpu.Get(); err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	copyCPU(oldCPU, cpu)
+			// 	time.Sleep(time.Duration(500) * time.Millisecond)
+			// }
+			// if err := cpu.Get(); err != nil {
+			// 	log.Fatal(err)
+			// }
+			// copyCPU(curCPU, cpu)
+			// fmt.Printf("oldCpu %v\n", oldCPU)
+			// fmt.Printf("curCpu %v\n", curCPU)
+			// cpu := 1 - (curCPU.Idle-oldCPU.Idle)/(curCPU.Total()-oldCPU.Total())
+			// fmt.Printf("cpu usage %v", cpu)
+
+			if err := mem.Get(pid); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("res mem %v(M)\n", (mem.Resident / (1024 * 1024)))
+			time.Sleep(time.Duration(1) * time.Second)
+		}
+	}
+
+}
