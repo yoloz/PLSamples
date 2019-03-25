f7d6c26db815a545ca75bf5ca0a06d6704d0f1a6
yoloz <z88897050@gmail.com>
2019-03-25 09:49:32 +0800

    lucene搜索例子

--- java/functions/lucene_ssdb/src/main/java/app/AppServer.java
+++ java/functions/lucene_ssdb/src/main/java/app/AppServer.java
@@ -30,31 +30,31 @@ public class AppServer {
      * @param args {@link String[]}
      */
     public static void main(String[] args) {
-        if (args == null || args.length != 1) {
-            System.err.println("启动失败,参数[" + Arrays.toString(args) + "]错误");
-            System.exit(1);
-        }
-        logger.info(args[0] + " starting...");
-        try {
-            List<Map<String, Object>> list = SqlliteUtil
-                    .query("select value from schema where name=?", args[0]);
-            if (list.isEmpty()) throw new LSException("index[" + args[0] + "] is not exit");
-            WriteIndex writeIndex = new WriteIndex(
-                    new Yaml().loadAs((String) list.get(0).get("value"), Schema.class));
-            Runtime.getRuntime().addShutdownHook(new Thread(writeIndex::close));
-            Utils.updateAppStatus(ManagementFactory.getRuntimeMXBean().getName().split("@")[0], args[0]);
-            writeIndex.write();
-            Utils.updateAppStatus("0", args[0]);
-        } catch (Exception e) {
-            logger.error(e.getCause() == null ? e.getMessage() : e.getCause());
-            System.err.println(e.getMessage());
-            try {
-                Utils.updateAppStatus("0", args[0]);
-            } catch (SQLException e1) {
-                logger.error(e1);
-            }
-            System.exit(1);
-        }
+//        if (args == null || args.length != 1) {
+//            System.err.println("启动失败,参数[" + Arrays.toString(args) + "]错误");
+//            System.exit(1);
+//        }
+//        logger.info(args[0] + " starting...");
+//        try {
+//            List<Map<String, Object>> list = SqlliteUtil
+//                    .query("select value from schema where name=?", args[0]);
+//            if (list.isEmpty()) throw new LSException("index[" + args[0] + "] is not exit");
+//            WriteIndex writeIndex = new WriteIndex(
+//                    new Yaml().loadAs((String) list.get(0).get("value"), Schema.class));
+//            Runtime.getRuntime().addShutdownHook(new Thread(writeIndex::close));
+//            Utils.updateAppStatus(ManagementFactory.getRuntimeMXBean().getName().split("@")[0], args[0]);
+//            writeIndex.write();
+//            Utils.updateAppStatus("0", args[0]);
+//        } catch (Exception e) {
+//            logger.error(e.getCause() == null ? e.getMessage() : e.getCause());
+//            System.err.println(e.getMessage());
+//            try {
+//                Utils.updateAppStatus("0", args[0]);
+//            } catch (SQLException e1) {
+//                logger.error(e1);
+//            }
+//            System.exit(1);
+//        }
 //        logger.info(args[0] + " finished...");
     }
 
