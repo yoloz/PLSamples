57a519422e0e15aadc301470ac6a8397848d93c3
yoloz <youloz@outlook.com>
2021-09-21 22:27:01 +0800

    moved to samples-java
    已经转移到samples-java中

--- java/functions/gatherData/bin/ga-restart.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-base_dir=`dirname $0`
-${base_dir}/ga-stop.sh
-${base_dir}/ga-start.sh -daemon
--- java/functions/gatherData/src/main/java/com/unimas/gatherdata/ConfigException.java
+++
@@ -1,9 +0,0 @@
-package com.unimas.gatherdata;
-
-public class ConfigException extends RuntimeException {
-
-    public ConfigException(String s) {
-        super(s);
-    }
-
-}
